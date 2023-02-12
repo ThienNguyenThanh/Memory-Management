4c01b10c713a0488cd1d54ccb457b290829b4d98
Thien Thanh <nguyenthanhthien0611@gmail.com>
2023-02-12 11:17:09 +0700

    Setup utils functions

--- memory/utils.py
+++ memory/utils.py
@@ -1,19 +1,8 @@
 from PIL import Image
 import pyrebase
 
-
-def resize_img():
-    fixed_height = 512
-    image = Image.open("triumph.png")
-    height_percent = (fixed_height / float(image.height))
-    width_size = int((float(image.width) * float(height_percent)))
-
-    image = image.resize((width_size, fixed_height))
-
-    image.save("triumph-resize.png")
-
-def upload_img():
-    firebaseConfig = {
+#CONFIG
+firebaseConfig = {
     "apiKey": "AIzaSyB-HRMYLqKT3nUHdJlOKpQJcbwFSRjp07E",
     "authDomain": "saritasa-python-test.firebaseapp.com",
     "databaseURL": "",
@@ -23,17 +12,29 @@ def upload_img():
     "appId": "1:894879225222:web:82c6503f51fdca1ff5c807"
     }
 
-    firebase = pyrebase.initialize_app(firebaseConfig)
-    storage = firebase.storage()
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage = firebase.storage()
 
-    path_on_cloud = "images/test1.jpg"
-    path_local = "s1000rr.png"
-    # storage.child(path_on_cloud).put(path_local)
+def resize_img(img_name):
+    fixed_height = 512   #height of carousel
+    image = Image.open(f"media/{img_name}")
+    height_percent = (fixed_height / float(image.height))
+    width_size = int((float(image.width) * float(height_percent)))
 
-    #get image
+    new_img = image.resize((width_size, fixed_height))
+
+    new_img.save(f"media/{img_name}")
+
+def upload_img(user_id, memory_id, img_name):
+    path_on_cloud = f"images/{user_id}/{memory_id}/{img_name}"
+    path_local = f"media/{img_name}"
+    storage.child(path_on_cloud).put(path_local)
+
+def get_img(user_id, memory_id, img_name):
     auth = firebase.auth()
     email = "Saritasa-Python-Test@gmail.com"
     password = "PassTest!"
     user = auth.sign_in_with_email_and_password(email, password)
+    path_on_cloud = f"images/{user_id}/{memory_id}/{img_name}"
     url = storage.child(path_on_cloud).get_url(user['idToken'])
-    print(url)
+    return url
