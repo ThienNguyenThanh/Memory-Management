238c881570dfb7ae21541aa9e82c2db308ffd0ca
Thien Thanh <nguyenthanhthien0611@gmail.com>
2023-02-14 00:33:42 +0700

    Setup create superuser from build.sh

---
+++ memory/management/commands/createsu.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+import os
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username=os.getenv('SUPER_USER_USERNAME'),
+                password=os.getenv('SUPER_USER_PASSWORD')
+            )
