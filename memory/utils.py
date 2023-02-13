2a4ba77a4480c49370a138f940331eb2914c79fb
thien <nguyenthanhthien0611@gmail.com>
2023-02-13 16:47:38 +0700

    Update Profile page & Add env variables

--- memory/utils.py
+++ memory/utils.py
@@ -1,15 +1,17 @@
-from PIL import Image
+import os
+
 import pyrebase
+from PIL import Image
 
 #CONFIG
 firebaseConfig = {
-    "apiKey": "AIzaSyB-HRMYLqKT3nUHdJlOKpQJcbwFSRjp07E",
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
     "authDomain": "saritasa-python-test.firebaseapp.com",
     "databaseURL": "",
     "projectId": "saritasa-python-test",
     "storageBucket": "saritasa-python-test.appspot.com",
     "messagingSenderId": "894879225222",
-    "appId": "1:894879225222:web:82c6503f51fdca1ff5c807"
+    "appId": os.getenv("FIREBASE_APP_ID")
     }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
@@ -32,8 +34,8 @@ def upload_img(user_id, memory_id, img_name):
 
 def get_img(user_id, memory_id, img_name):
     auth = firebase.auth()
-    email = "Saritasa-Python-Test@gmail.com"
-    password = "PassTest!"
+    email = os.getenv("FIREBASE_STORAGE_EMAIL")
+    password = os.getenv("FIREBASE_STORAGE_PASSWORD")
     user = auth.sign_in_with_email_and_password(email, password)
     path_on_cloud = f"images/{user_id}/{memory_id}/{img_name}"
     url = storage.child(path_on_cloud).get_url(user['idToken'])
--- social/views.py
+++ social/views.py
@@ -1,9 +1,30 @@
+import json
+
+import folium
+import geocoder
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from memory.models import Memory
 
 
 @login_required
 def profile_view(request):
-    return render(request, 'social/profile.html')
+    form = None
+
+    # Create Map Object
+    m = folium.Map(location=[0, 0], zoom_start=2)
+
+    user_mem = Memory.objects.filter(user=request.user)
+    for mem in user_mem:
+        folium.Marker([mem.lat, mem.lng], tooltip='Click for more',
+                  popup=mem.location).add_to(m)
+                  
+    # Get HTML Representation of Map Object
+    m = m._repr_html_()
+    context = {
+        'm': m
+    }
+    return render(request, 'social/profile.html', context)
 
 
