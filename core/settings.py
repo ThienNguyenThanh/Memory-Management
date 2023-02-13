4ae8b353200f88c5bcdb9439300d943644fd6622
thien <nguyenthanhthien0611@gmail.com>
2023-02-13 13:40:29 +0700

    Fix deployment issue

--- core/settings.py
+++ core/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
